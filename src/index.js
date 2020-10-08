a71dddd4b8bacdd7731089d90d298db90996c99f
Bruno Gaspar <bruninhoogaspar@gmail.com>
2020-10-08 09:32:15 -0300

    Alterado nome da estratégia

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ const jwtHapiAuth = require("hapi-auth-jwt2");
 const server = require("./server");
 const routes = require("./routes");
 
-const { name, schema, options } = require("./auth/strategies/jwt");
+const jwtStrategy = require("./auth/strategies/jwt");
 
 const init = async () => {
   server.route(routes);
 
   await server.register(jwtHapiAuth);
 
-  server.auth.strategy(name, schema, options);
+  server.auth.strategy(jwtStrategy.name, jwtStrategy.schema, jwtStrategy.options);
 
-  server.auth.default(name);
+  server.auth.default(jwtStrategy.name);
 
   await server.start().then(console.log("\nServer running on %s\n", server.info.uri));
 };
